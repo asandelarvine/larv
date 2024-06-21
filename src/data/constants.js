31f6fa8fea1ccc5a84d09302e1a6b9eab740b388
Asande Larvine <larvine.asande@student.moringaschool.com>
2024-06-21 09:03:52 +0300

    Update constants.js cv link changed

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
       "I am an Enthusiastic developer. Passionate about building delightful and impactful products that make a difference. Writer on Medium. Excited about tough problems, and always seeking discomfort to learn something new. I’m a self-driven, motivated, and independent learner.",
     github: "https://github.com/asandelarvine",
     resume:
-      "https://drive.google.com/file/d/1F9sGUSo0aUdn91WnzQmpq8TV4n16pbv6/view?usp=sharing",
+      "https://drive.google.com/file/d/1KNJXknwzxZJh0_apZpOKoItH1--naFXz/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/larvine-asande-a51214230/",
   };
   
